f87117777d4ad3a3343254631a2d6fd2b698e142
Omar Abdelmotaleb <oabdelmo@stevens.edu>
2024-05-06 02:25:58 -0400

    Updates

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from django.shortcuts import render, redirect
+from .models import ClickCount
+
 def index(request):
-    return HttpResponse("Webapp index.")
+    if request.method == 'POST':  # If the button was clicked
+        obj, created = ClickCount.objects.get_or_create(id=1)
+        obj.count += 1
+        obj.save()
+        return redirect('index')  # Reload the page
+
+    obj, created = ClickCount.objects.get_or_create(id=1)
+    context = {'count': obj.count}
+    return render(request, 'counter/index.html', context)
